8ea90ea3e1b6e7b4f1430c1e88656316898b97a1
messierr <>
2021-11-30 10:29:32 +0300

    chast modul

--- WorldCinema/ViewController.swift
+++ WorldCinema/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputlogin: UITextField!
+    
+    @IBOutlet weak var inputpass: UITextField!
+    
+    var userDef = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("You token is:\(userDef.value(forKey: "token"))")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func auth(_ sender: Any) {
+        guard !inputlogin.text!.isEmpty else {
+            return showAlertDialog(message: "Проверьте логин")
+        }
+        guard !inputpass.text!.isEmpty else {
+            return showAlertDialog(message: "Проверьте пароль")
+        }
+        let url = "http://cinema.areas.su/auth/login=\(inputlogin.text!)&password\(inputpass.text!)"
+        AF.request(url, method: .post).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let jsonToken = json["notice"]["token"].stringValue
+                self.userDef.setValue(json, forKey: "token")
+            case .failure(let error):
+                self.showAlertDialog(message: error.localizedDescription)
+            }
+        }
+        
+//        performSegue(withIdentifier: "auth", sender: nil)
+    }
+    
+    
+    func showAlertDialog(message: String) {
+        let alert = UIAlertController(title: "Ошибка", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
---
+++ WorldCinema/cinemavid.swift
@@ -0,0 +1,11 @@
+//
+//  cinemavid.swift
+//  WorldCinema
+//
+//  Created by 35 кабинет on 30.11.2021.
+//
+
+import Foundation
+struct Res {
+    
+}
